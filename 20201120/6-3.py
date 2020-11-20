29c40611bc819e3ad0265c7a233ce0f97691f3d5
Eok99 <car33777@gmail.com>
2020-11-20 09:35:23 +0900

    20201120 일일커밋
    반복문. for, while

---
+++ 20201120/6-3.py
@@ -0,0 +1,10 @@
+#while
+
+customer = "토르"
+index = 5
+while index >= 1:
+    print("{0}, 커피가 준비되었습니다.{1}번 남았어요.".format(customer, index))
+    index -= 1
+    if index == 0:
+        print("커피는 폐기 처분 되었습니다."
+        
