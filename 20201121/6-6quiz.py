ada802f8710f5c6eb472f0e0c25ca2fc422ed1e2
Eok99 <car33777@gmail.com>
2020-11-23 10:16:10 +0900

    6.퀴즈완성
    20201123 일일커밋.
    반복문 퀴즈
    랜덤,for if, else,format

--- 20201121/6-6quiz.py
+++ 20201121/6-6quiz.py
@@ -13,14 +13,13 @@
 
 # 총 탑승 승객 : 2분
 from random import*
-time = range(5,51)
-waiting = sample(time,1) 
-
-
-
-for customer in range(1,51): 
-  
-    if 15 < waiting and waiting >5 :
-     print("1")
+cnt = 0
+for i in range(1,51):
+    time = randrange(5,51)
+    if 5<= time and time <=15:
+     print("[0]{0}번째 손님(소요시간:{1}분)".format(i,time))
+     cnt += 1
     else:
-     print("2")
+     print("[ ]{0}번째 손님(소요시간:{1}분)".format(i,time))
+     
+print("총 탑승 승객 : {0}분".format(cnt))
