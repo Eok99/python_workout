5eb17971eef67e600f1ac5066c079f379c46b712
Eok99 <car33777@gmail.com>
2020-11-26 17:10:48 +0900

    Update 7quiz.py

--- 20201125/7quiz.py
+++ 20201125/7quiz.py
@@ -24,7 +24,7 @@ def std_weight(height, gender):
         return height * height * 22
     else:
         return height * height * 21
-height = 160
+height = 162
 gender = "여자"
 weight = round(std_weight(height/100,gender),2)
 print("키 {0}cm {1}의 표준 체중은 {2}입니다.".format(height,gender,weight))
