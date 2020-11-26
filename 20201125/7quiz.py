333451153db179a95d299653a06376eb3a9dfd40
Eok99 <car33777@gmail.com>
2020-11-26 17:09:57 +0900

    20201116일일커밋
    함수 퀴즈

--- 20201125/7quiz.py
+++ 20201125/7quiz.py
@@ -11,4 +11,20 @@
 # 조건 2: 표준체중 소수 둘째자리 까지 표시
 # ex 키 175cm 남자의 표준 체중은 67.39kg 입니다
 
-def std_weight()
+
+# 내가 처음에 만든거
+# def std_weight(height, gender):
+#     std_weight = round(height * height * 22 /10000,2)
+#     print("키 {0} {1}의 표준 체중은 {2}입니다.".format(height,gender,std_weight))
+# std_weight(173,"남자")
+
+# 컨닝 슬쩎한번봄
+def std_weight(height, gender):
+    if gender == "남자":
+        return height * height * 22
+    else:
+        return height * height * 21
+height = 160
+gender = "여자"
+weight = round(std_weight(height/100,gender),2)
+print("키 {0}cm {1}의 표준 체중은 {2}입니다.".format(height,gender,weight))
